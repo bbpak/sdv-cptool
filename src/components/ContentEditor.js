16e8d691530eb0e62808d93dfde6bd838ec90fa2
Bouhm <bbouhm@gmail.com>
2018-05-08 22:06:16 -0700

    restart from scratch

---
+++ src/components/ContentEditor.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import _ from 'lodash'
+
+export default class ContentEditor extends Component {
+  constructor() {
+    super()
+    this.state = {
+    }
+  }
+
+
+
+  render() {
+    return (
+      <div className="content-editor">
+      </div>
+    )
+  }
+}
