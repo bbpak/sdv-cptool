1fbf4d2cf148560d6513febd0479854b0a91453a
Bouhm <bbouhm@gmail.com>
2018-05-26 17:55:30 -0700

    tab fix

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -11,23 +11,36 @@ export default class Tabs extends Component {
   }
 
   handleTabClick = i => {
-    this.setState({activeTabIndex: i})
+    if (this.state.activeTabIndex == null)
+      this.setState({activeTabIndex: i})
+    else 
+      this.setState({activeTabIndex: null})
   }
 
   renderTabs = (tabs) => {
+    const { activeTabIndex } = this.state
     return (
       <div className="tabs">
-        {_.map(tabs, (tab, i) => {
-          return (
-            <div 
-              key={i} 
-              className="tab"
-              onClick={() => this.handleTabClick(i)}
-            >
-              {tab.label}
-            </div>
-          )
-        })}
+        {this.state.activeTabIndex == null 
+          ? 
+          _.map(tabs, (tab, i) => {
+            return (
+              <div 
+                key={i} 
+                className="tab"
+                onClick={() => this.handleTabClick(i)}
+              >
+                {tab.label}
+              </div>
+            )
+          }) 
+        : 
+        <div 
+          className="tab tab-active"
+          onClick={() => this.handleTabClick(activeTabIndex)}
+        >
+          {tabs[activeTabIndex].label}
+        </div>}
       </div>
     )
   }
@@ -37,11 +50,11 @@ export default class Tabs extends Component {
     const { activeTabIndex } = this.state
 
     return (
-      <div className="tabs-container">
+      <div className="tabs-container" style={activeTabIndex == null ? { flex: 0 } : { flex: '0 1 50%' }}>
         {this.renderTabs(tabs)}
         <div 
           className="tab-content"
-          style={activeTabIndex == null ? { flex: 0 } : { flex: '0 1 50%' }}
+          style={activeTabIndex == null ? { display: 'none' } : { display: 'block' }}
         >
           {activeTabIndex !== null && tabs[activeTabIndex].content}
         </div>
