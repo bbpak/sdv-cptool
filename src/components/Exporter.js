d48dc51cb4719a5ca246cd62a484f164a3dcf27a
bbpak <bbouhm@gmail.com>
2018-06-02 02:35:41 -0700

    fix export css

--- src/components/Exporter.js
+++ src/components/Exporter.js
@@ -49,52 +49,48 @@ class Exporter extends Component {
       <div className="panel exporter">
         <pre className="header disabled">Manifest</pre>
         <div className="editor manifest-form">
-        <FieldBlock
-          className="disabled"
-          style={{border: 'none', margin: 0}}
-          blockData={manifestData}
-          handleBlockDataChange={this.handleBlockDataChange}
-        />
-      </div>
-      <br />
-      <pre className="header disabled">Config</pre>
-      <div className="editor config-form">
-      <FieldBlock
-        className="disabled"
-        style={{border: 'none', margin: 0}}
-        blockData={{}}
-        handleBlockDataChange={this.handleBlockDataChange}
-      >
-        <div
-        className="field-block add-button"
-        style={{ cursor: 'pointer', border: 'none', margin: 0 }}
-        onClick={this.handleAddBlock}
-      >
-        <i title="new block" className="material-icons add">
-          {'add'}
-        </i>
-      </div>
-    </FieldBlock>
-    </div>
-    <br />
-      <span style={{display: 'flex'}}>
-      <div className="button" onClick={this.handleExportData}>
-      <i
-      style={{verticalAlign: 'middle'}}
-        className="material-icons"
-      >
-        {'get_app'}
-      </i> Export content.json
-    </div>
-      <div className="button disabled" onClick={this.handleExportData}>
-      <i
-      style={{verticalAlign: 'middle'}}
-        className="material-icons"
-      >
-        {'get_app'}
-      </i> Export bundle
-    </div>
-    </span>
+          <FieldBlock
+            className="disabled"
+            style={{ border: 'none', margin: 0 }}
+            blockData={manifestData}
+            handleBlockDataChange={this.handleBlockDataChange}
+          />
+        </div>
+        <br />
+        <pre className="header disabled">Config</pre>
+        <div className="editor config-form">
+          <FieldBlock
+            className="disabled"
+            style={{ border: 'none', margin: 0 }}
+            blockData={{}}
+            handleBlockDataChange={this.handleBlockDataChange}
+          >
+            <div
+              className="field-block add-button"
+              style={{ cursor: 'pointer', border: 'none', margin: 0 }}
+              onClick={this.handleAddBlock}
+            >
+              <i title="new block" className="material-icons add">
+                {'add'}
+              </i>
+            </div>
+          </FieldBlock>
+        </div>
+        <br />
+        <span style={{ display: 'flex' }}>
+          <div className="button" onClick={this.handleExportData}>
+            <i style={{ verticalAlign: 'middle' }} className="material-icons">
+              {'get_app'}
+            </i>{' '}
+            Export content.json
+          </div>
+          <div className="button disabled" onClick={this.handleExportData}>
+            <i style={{ verticalAlign: 'middle' }} className="material-icons">
+              {'get_app'}
+            </i>{' '}
+            Export bundle
+          </div>
+        </span>
       </div>
     )
   }
